c02cc8513ac7f95c1f1897a28a3b237dd1a3e532
Samiksha <gaursamiksha37@gmail.com>
2024-01-31 22:50:38 +0530

    hard_assertions

--- DemoPytest/test_assertions_hard.py
+++ DemoPytest/test_assertions_hard.py
@@ -9,19 +9,16 @@ class AssertionsTest():
 def test_lambdatest_radio_button_demo_value():
     driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.get()
-    driver.find_element(By.XPATH,
-                        "//h4[contains(text(),'Gender')]//following::input[@value='Male'])").click()
-    driver.find_element(By.XPATH,
-                        "//h4[contains(text(),'')]//following::input[@value='15 - 50'])").click()
-    driver.find_element(By.XPATH, "//button[text()='Get values']").click()
-    gender = driver.find_element(By.CSS_SELECTOR, ".genderbutton").text
-    age_group = driver.find_element(By.CSS_SELECTOR, ".groupradiobutton").text
+    driver.get("https://www.lambdatest.com/selenium-playground/radiobutton-demo")
+    driver.find_element(By.XPATH,"//h4[contains(text(),'Gender')]//following::input[@value='Male']").click()
+    driver.find_element(By.XPATH,"//h4[contains(text(),'Age')]//following::input[@value='15 - 50']").click()
+    driver.find_element(By.XPATH,"//button[text()='Get values']").click()
+    gender = driver.find_element(By.CSS_SELECTOR,".genderbutton").text
+    age_group = driver.find_element(By.CSS_SELECTOR,".groupradiobutton").text
     print("Gender Object: \t", id(gender))
     print("Male Object: \t", id("Male"))
+
 
     assert gender == "Male", "Gender Is Not Correct"
     assert driver.title.__contains__("Selenium Grid Online")
     assert "51" in age_group, "Age Group is not Correct"
-
-
