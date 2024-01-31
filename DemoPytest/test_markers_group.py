6766aa05bf6f94b0e4037834d5b3ddcef8114360
Samiksha <gaursamiksha37@gmail.com>
2024-02-01 01:17:48 +0530

    markers-group

---
+++ DemoPytest/test_markers_group.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
+def test_ajax_form_submit():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.lambdatest.com/selenium-playground/ajax-form-submit-demo")
+
+    driver.find_element(By.XPATH, "//input[@id='title']").send_keys("Samiksha")
+    driver.find_element(By.XPATH, "//textarea[@id='description']").send_keys("Samiksha is learning PyTest Framework.")
+    driver.find_element(By.XPATH, "//input[@id='btn-submit']").click()
+    request = driver.find_element(By.ID, "submit-control").text
+    assert request.__contains__("Processing")
+
+
+def test_e2e():
+    print("End to End Test")
+
+
+def test_login():
+    print("Login to application.")
+
+
+def test_logout():
+    print("Logout")
+
